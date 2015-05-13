67e1ab6ad5c14894d61afa670a6426d812f28a61
Viktor_j <vicpride@gmail.com>
2015-05-13 13:41:57 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,78 @@
+<?php
+
+header('Content-type: text/html; charset=utf-8');
+error_reporting(E_ALL);
+// Lesson 3
+
+//echo date('d.m.Y H:i:s', mktime(0,0,0,1,1,70))."\n";       //print zero of timestemp
+
+//$date = array();
+//mt_srand(time());
+//for ($i = 0; $i <= 4; $i++) {              //fill $date by 5 element of random timestamp
+//    $date[$i] = mt_rand(0, time());
+//}
+//
+////SOLUTION #1
+//foreach ($date as $value) {                //print $date in data-format
+//    echo date('D, d.m.Y H:i:s', $value) . "\n";
+//}
+//$days = array();                            
+//$months = array(); 
+//
+//foreach ($date as $value) {                 
+//    $days[] = (int) date('d', $value);      //parse $date by day in $days
+//    $months[] = (int) date('m', $value);    //parse $date by month in $months
+//}
+//echo "<br>Min day: ".min($days)."\n";       //print min of $days
+//
+//$month = max($months);
+//echo "<br>Max month: ".$month.'='. date('F', mktime(0,0,0,$month,1))."\n";          //print max of $month
+
+////SOLUTION #2
+//
+//foreach ($date as $value) {                         //print $date in data-format
+//    echo date('D, d.m.Y H:i:s', $value) . "\n";
+//}
+//echo"____________________\n";
+//
+//function sortByDay($a,$b)                           
+//{
+//    if ((int)date('d',$a) == (int)date('d',$b)){
+//        return 0;
+//    }
+//    return ((int)date('d',$a) < (int)date('d',$b)) ? -1 : 1;
+//}
+//function sortByMonth($a,$b)
+//{
+//    if ((int)date('m',$a) == (int)date('m',$b)){
+//        return 0;
+//    }
+//    return ((int)date('m',$a) < (int)date('m',$b)) ? -1 : 1;
+//}
+//
+//usort($date, "sortByDay");
+//echo "<br>Min day: ".(int)date('d',$date[0])."\n";
+//
+//usort($date, "sortByMonth");
+//echo "<br>Max month: ".(int)date('m',$date[count($date)-1])."=".date('F',$date[count($date)-1])."\n";
+
+//foreach ($date as $value) {                //print $date in data-format
+//    echo date('D, d.m.Y H:i:s', $value) . "\n";
+//}
+
+//sort($date);                                //sort $date from lowest to highest
+////echo var_dump($date);
+//
+//$selected = array_pop($date);               //cut last element $date to $selected
+////echo var_dump($date);
+////echo var_dump($selected);
+//
+//echo "<br>\$selected = " . date('d.m.Y H:i:s', $selected)."\n";    //print $selected in data-format
+//
+//date_default_timezone_set('America/New_York');
+//echo "<br>\$selected (in NY) = " . date('d.m.Y H:i:s', $selected)."\n";  //print $selected in NY-timezone
+//echo "<br>New Timezone is ".  date_default_timezone_get();
+//
+//?>
+
+<script src="main.js"></script>
---
+++ main.js
@@ -0,0 +1,51 @@
+//SOLUTION #2
+
+var $date = new Array(5);
+
+for(i=0; i<5; i++){
+  var time = Math.floor((Math.random() * (Date.now() - 1) + 1));
+  $date[i] = new Date (time);
+}
+
+
+$date.forEach(function(entry) {
+    document.write(entry.toString()+"<br>");
+});
+
+function sortByDay($a,$b)                           
+{
+    if ($a.getDay() == $b.getDay()){
+        return 0;
+    }
+    return ($a.getDay() < $b.getDay()) ? -1 : 1;
+}
+
+function sortByDate($a,$b)                           
+{
+    if ($a.getDate() == $b.getDate()){
+        return 0;
+    }
+    return ($a.getDate() < $b.getDate()) ? -1 : 1;
+}
+
+function sortByMonth($a,$b)                           
+{
+    if ($a.getMonth() == $b.getMonth()){
+        return 0;
+    }
+    return ($a.getMonth() < $b.getMonth()) ? -1 : 1;
+}
+
+$date.sort(sortByDay);
+document.write("<br>Min day: "+$date[0].toDateString()+"<br>");
+$date.sort(sortByMonth);
+document.write("Max Month: "+$date[4].getMonth()+"<br>");
+
+$date.sort(sortByDate);
+var $selected = $date.pop();
+document.write("$selected: "+$selected.getDate()+"."+$selected.getMonth()+
+    "."+$selected.getFullYear()+" "+$selected.getHours()+":"+$selected.getMinutes()+":"+
+      $selected.getSeconds()+"<br>");
+      
+document.write($selected.getTimezoneOffset()/60+"<br>")
+document.write($selected.toTimeZone()+"<br>")
